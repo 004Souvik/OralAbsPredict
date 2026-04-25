16c3d5d547315036720ab1b85c3012d4da57ccba
004Souvik <123533712+004Souvik@users.noreply.github.com>
2026-04-25 22:37:32 +0530

    Update OralAbsPredict.py

--- OralAbsPredict.py
+++ OralAbsPredict.py
@@ -393,7 +393,7 @@ elif st.session_state.page == "contact":
         footer {visibility: hidden;}
         </style>
         <div class="footer">
-        <p>Found an issue? email at souvikpore123@gmail.com with subject 'HIA-HOB Predictor Bug'. Include your OS, Python version, and a description of the problem.</p>
+        <p>Found an issue? email at souvikpore123@gmail.com with subject 'HIA-HOB Predictor Bug'.</p>
         </div>
         """
     st.markdown(hide_footer_style, unsafe_allow_html=True)
